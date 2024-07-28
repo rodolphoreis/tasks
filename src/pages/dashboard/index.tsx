6052d2a014242674b4454321ac661c53f1ecfcae
Rodolpho Reis <rodolphoreis@live.com>
2024-07-28 10:21:23 +0100

    feat: create component and style

---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,14 @@
+import styles from "./dashboard.module.css";
+import "../../styles/globals.css";
+
+const Dashboard = () => {
+  return (
+    <>
+      <div className={styles.container}>
+        <h2 className={styles.title}>dashboard</h2>
+      </div>
+    </>
+  );
+};
+
+export default Dashboard;
