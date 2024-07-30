820da7e2ce21344ecd78f2f8b878da10252efadb
Rodolpho Reis <rodolphoreis@live.com>
2024-07-30 18:48:04 +0100

    chore: create register ("use server")

---
+++ app/(auth)/register/_actions/register.ts
@@ -0,0 +1,39 @@
+"use server";
+
+import db from "@/src/lib/db";
+import { hashSync } from "bcrypt-ts";
+
+export default async function register(formData: FormData) {
+  const name = formData.get("name");
+  const email = formData.get("email");
+  const password = formData.get("password");
+
+  if (
+    typeof name !== "string" ||
+    name.trim() === "" ||
+    typeof email !== "string" ||
+    email.trim() === "" ||
+    typeof password !== "string" ||
+    password.trim() === ""
+  ) {
+    throw new Error("Todos os campos são obrigatórios.");
+  }
+
+  const user = await db.user.findUnique({
+    where: { email },
+  });
+
+  if (user) {
+    throw new Error("Email já cadastrado.");
+  }
+
+  await db.user.create({
+    data: {
+      name: name.trim(),
+      email: email.trim(),
+      password: hashSync(password.trim()),
+    },
+  });
+
+  return { redirect: "/" };
+}
