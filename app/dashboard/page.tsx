2ff51af3d3fe13860e5195c8e966c9292efea7f1
Rodolpho Reis <rodolphoreis@live.com>
2024-08-06 18:46:47 +0100

    feat: create types

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -8,19 +8,39 @@ export const metadata = {
   title: "Tasks - Dashboard",
   description: "Painel de controle",
 };
+
+type User = {
+  name: string;
+  email: string;
+  image?: string;
+};
+
+type Session = {
+  user?: User;
+  expires: string;
+};
+
 const Dashboard = async () => {
   const session = await auth();
+  console.log(session);
 
-  if (!session?.user) {
+  if (!session || !session.user) {
     return redirect("/login");
   }
 
+  const normalizedSession: Session = {
+    ...session,
+    user: {
+      name: session.user.name || "",
+      email: session.user.email || "",
+      image: session.user.image || "",
+    },
+  };
+
   return (
-    <>
-      <div className={styles.container}>
-        <DashboardComponent />
-      </div>
-    </>
+    <div className={styles.container}>
+      <DashboardComponent session={normalizedSession} />
+    </div>
   );
 };
 
