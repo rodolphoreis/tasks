1782ef1ada73faba098353765aa172fb7796b2d5
Rodolpho Reis <rodolphoreis@live.com>
2024-08-06 17:11:31 +0100

    refactor: refactor component

--- src/components/textarea/index.tsx
+++ src/components/textarea/index.tsx
@@ -1,32 +1,12 @@
 "use client";
 
-import { HTMLProps, useState } from "react";
+import { HTMLProps } from "react";
 import styles from "./textarea.module.css";
 
-interface TextareaProps
-  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
-
-const Textarea: React.FC<TextareaProps> = ({
-  ...rest
-}: HTMLProps<HTMLTextAreaElement>) => {
-  const [input, setInput] = useState<string>("");
-
-  const handleTextareaChange = (
-    event: React.ChangeEvent<HTMLTextAreaElement>
-  ) => {
-    event.preventDefault();
-    setInput(event.target.value);
-  };
-
+const Textarea = ({ ...rest }: HTMLProps<HTMLTextAreaElement>) => {
   return (
     <>
-      <textarea
-        name="input"
-        placeholder="Texto"
-        className={styles.textarea}
-        onChange={handleTextareaChange}
-        {...rest}
-      ></textarea>
+      <textarea name="input" className={styles.textarea} {...rest}></textarea>
     </>
   );
 };
